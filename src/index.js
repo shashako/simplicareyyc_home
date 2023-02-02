8b47a68d112aefade53e8d7e54d4b7511107dce7
Duc Huynh <duc.huynhcong@gmail.com>
2023-02-02 22:50:10 +0700

    ##ok

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {Home} from "./containers/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+ 
 const root = createRoot(document.getElementById('root'));
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
-          
+        <Route path="/simplicareyyc_home/" element={<Home />}>          
         </Route>
       </Routes>
     </BrowserRouter>
