9cb9cd1c60f0df01cea5fa339e989c8eb246992a
Duc Huynh <duc.huynhcong@gmail.com>
2023-02-06 00:38:39 +0700

    update UI

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -3,7 +3,9 @@ import { Container, Row } from "react-bootstrap";
 
 function iframe() {
     return {
-        __html: '<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7866.040192265814!2d105.5723145!3d9.6793236!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1675613848869!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+        __html: `
+    <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7866.040192265814!2d105.5723145!3d9.6793236!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1675613848869!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    `
     }
 }
 export class  Contact extends React.Component { 
